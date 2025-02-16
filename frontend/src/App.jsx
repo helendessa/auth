f5586256a402d3ae53d46527ee269a761fdea17c
helendessa <helendessa@gmail.com>
2025-02-16 00:15:13 -0300

    add frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,10 @@
+// filepath: /home/helen/Documents/projetos/auth/frontend/src/App.jsx
+function App() {
+  return (
+    <div className="text-3xl font-bold underline text-red-500">
+      Hello world!
+    </div>
+  )
+}
+
+export default App;
